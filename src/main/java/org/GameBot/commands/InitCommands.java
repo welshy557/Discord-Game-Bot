72de4909c473db57862a235abed95cf0c13eb792
Liam Welsh <liamwelsh17@gmail.com>
2022-11-23 18:35:00 -0400

    Inits commands globally if in prod env

--- src/main/java/org/GameBot/commands/InitCommands.java
+++ src/main/java/org/GameBot/commands/InitCommands.java
@@ -1,38 +1,74 @@
 package org.GameBot.commands;
 
+import net.dv8tion.jda.api.entities.EmbedType;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InitCommands extends ListenerAdapter {
     @Override
     public void onGuildReady(@NotNull GuildReadyEvent event) {
-        List<CommandData> commandData = new ArrayList<>();
+        // If in dev env
+        if (System.getenv().get("TOKEN") == null) {
+            List<CommandData> commandData = new ArrayList<>();
 
-        // quiz commands
-        commandData.add(Commands.slash("quiz", "Play a quiz game").addOption(OptionType.STRING, "questions", "The number of questions"));
-        commandData.add(Commands.slash("close-quiz", "Close your current quiz"));
+            // quiz commands
+            commandData.add(Commands.slash("quiz", "Play a quiz game").addOption(OptionType.STRING, "questions", "The number of questions"));
+            commandData.add(Commands.slash("close-quiz", "Close your current quiz"));
 
-        // guess commands
-        commandData.add(Commands.slash("guess", "Play a guessing game").addOption(OptionType.STRING, "guesses", "The number of guesses allowed"));
-        commandData.add(Commands.slash("close-guess", "Close your current guess game"));
+            // guess commands
+            commandData.add(Commands.slash("guess", "Play a guessing game").addOption(OptionType.STRING, "guesses", "The number of guesses allowed"));
+            commandData.add(Commands.slash("close-guess", "Close your current guess game"));
 
-        // ConnectFour commands
-        commandData.add(Commands.slash("connect-four", "Play a game of Connect 4 with a friend").addOption(OptionType.MENTIONABLE, "opponent", "Opponent to play against"));
-        commandData.add(Commands.slash("close-connect-four", "Close your current guess game").addOption(OptionType.MENTIONABLE, "opponent", "Opponent you played against"));
-        // Sending commands to guild
+            // ConnectFour commands
+            commandData.add(Commands.slash("connect-four", "Play a game of Connect 4 with a friend").addOption(OptionType.MENTIONABLE, "opponent", "Opponent to play against"));
+            commandData.add(Commands.slash("close-connect-four", "Close your current guess game").addOption(OptionType.MENTIONABLE, "opponent", "Opponent you played against"));
+            // Sending commands to guild
 
-        // Help command
-        commandData.add(Commands.slash("help", "Link to README"));
+            // Help command
+            commandData.add(Commands.slash("help", "Link to README"));
 
-        event.getGuild().updateCommands().addCommands(commandData).queue();
+            event.getGuild().updateCommands().addCommands(commandData).queue();
+        }
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        // If in production env
+        if (System.getenv().get("TOKEN") != null) {
+            List<CommandData> commandData = new ArrayList<>();
+
+            // quiz commands
+            commandData.add(Commands.slash("quiz", "Play a quiz game").addOption(OptionType.STRING, "questions", "The number of questions"));
+            commandData.add(Commands.slash("close-quiz", "Close your current quiz"));
+
+            // guess commands
+            commandData.add(Commands.slash("guess", "Play a guessing game").addOption(OptionType.STRING, "guesses", "The number of guesses allowed"));
+            commandData.add(Commands.slash("close-guess", "Close your current guess game"));
+
+            // ConnectFour commands
+            commandData.add(Commands.slash("connect-four", "Play a game of Connect 4 with a friend").addOption(OptionType.MENTIONABLE, "opponent", "Opponent to play against"));
+            commandData.add(Commands.slash("close-connect-four", "Close your current guess game").addOption(OptionType.MENTIONABLE, "opponent", "Opponent you played against"));
+            // Sending commands to guild
+
+            // Help command
+            commandData.add(Commands.slash("help", "Link to README"));
+
+            event.getJDA().updateCommands().addCommands(commandData).queue();
+        }
+
     }
 
     @Override
